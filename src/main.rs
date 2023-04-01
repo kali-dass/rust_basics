104b396fe1ae79f2346b940d2a306cfe68b75185
Kalidass Mookkaiah <kalidass@gmail.com>
2023-04-01 23:12:19 +0100

    immutable, shadowing and type annotation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    
+    // imutables
+    let x = 5;
+    println!("value of x is {x}");
+    // can't assign values to immutable variable
+    //x = 6;
+    //println!("value of x is {x}");
+
+    // mutable 
+    let mut y = 5;
+    println!("value of y is {y}");
+    // assign values to mutable variable
+    y = 6;
+    println!("value of y is {y}");
+
+    //shadow
+    let x = x +1;
+    {
+        let x = x +1;
+        println!("value of x in inner scope {x}");
+    }
+    println!("value of x in outter scope {x}");
+
+    // specify data type
+    let guess: u32 = "42".parse().expect("Not a number");
+    println!(" guess {guess}");
 }
